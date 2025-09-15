ad514612f31fd35f5c501b20a87f47ce6ca0226f
Muhamad Fachri Alifiandi <aliffachrimuhamad@gmail.com>
2025-09-15 21:47:23 +0700

    projek mini edit

--- Pig/main.py
+++ Pig/main.py
@@ -1,27 +1,58 @@
 import random
+import os
+
+os.system('cls')
 
 
 def roll():
     min_value = 1
     max_value = 6
-    roll = random.ranch(min_value, max_value)
+    roll = random.randint(min_value, max_value)
 
     return roll
+
+
 while True:
-    players = input("Enter the number of the players (1-4): ")
+    players = input("Enter the number of the players (2-4): ")
     if (players.isdigit()):
         players = int(players)
         if (1 <= players <= 4):
             break
-        else: print("Must be beetween 2-4 players.")
-        
-    else: print("Invalid, try again.")
+        else:
+            print("Must be beetween 2-4 players.")
 
-print(players)
+    else:
+        print("Invalid, try again.")
+
 
 max_score = 50
 players_scores = [0 for _ in range(players)]
 
+while max(players_scores) < max_score:
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "Turn has just started!\n")
+        print("Your total score is : ", players_scores[player_idx], "\n")
+        current_score = 0
 
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if (should_roll.lower()) != 'y':
+                break
 
+            value = roll()
+            if value == 1:
+                print('YOU ROLLED A 1! Turn done!')
+                current_score = 0
+            else:
+                current_score += value
+                print("You rolled a:", value)
 
+            print("Your score is :", current_score)
+
+        players_scores[player_idx] += current_score
+        print("Your total score is :", players_scores[player_idx])
+
+max_score = max(players_scores)
+winning_idx = players_scores.index(max_score)
+print("\nPlayer number", winning_idx + 1,
+      "is the winner with a score of: ", max_score)
